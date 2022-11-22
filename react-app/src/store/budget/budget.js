7d4dd1946665cfd5b1d6e7e999f755fac6b4d910
Eric-Peltzman33 <erpeltz@gmail.com>
2022-11-22 13:59:53 -0500

    update budget

---
+++ react-app/src/store/budget/budget.js
@@ -0,0 +1,25 @@
+const update_budgets = 'budgets/update_budget';
+const load_budgets = 'budgets/load_budgets';
+
+const update = (budget) => {
+    return { type: update_budgets, budget };
+};
+
+const load = (budgets) => {
+    return { type: load_budgets, budgets };
+}
+
+export const updateBudget = (data) => async (dispatch) => {
+    const response = await fetch(`/ap[i/budgets/${data.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    if (response.ok) {
+        const budget = await response.json();
+        dispatch(update(budget));
+        return budget;
+    }
+};
