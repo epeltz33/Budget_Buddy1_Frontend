0d06d08a1108b95240d195f53b8dc5e67990f9b8
Eric Peltzman <epeltz@optonline.net>
2022-11-22 02:48:18 +0000

    get transaction by the date wow this was hard

--- react-app/src/store/transaction/selectors.js
+++ react-app/src/store/transaction/selectors.js
@@ -18,4 +18,23 @@ export const getTransactionsByCategory = createSelector(
     }
 );
 
+export const getTransactionsByDate = ( [transactions, ( state, dateQuery ) => dateQuery],
+	( transactions, dateQuery ) => {
+		const isInRange = dateQuery.indexOf( "&" ) !== -1; // the "&" is used to indicate a range of dates so !== -1 means that it is in the string of the ranges
+		const firstDateYear = ( isInRange ) ? parseInt( dateQuery.split( "&" )[0].slice( 0, 4 ) ) : parseInt( dateQuery.slice( 0, 4 ) ); // the slice (0, 4) is used to get the year from the date string (YYYY-MM-DD)
+		const firstDateMonth = ( isInRange ) ? parseInt( dateQuery.split( "&" )[0].slice( 4, 6 ) ) : parseInt( dateQuery.slice( 4, 6 ) ); // the slice (4, 6) is used to get the month from the date string (YYYY-MM-DD) so 4 is the start and 6 is the end (so it doesn't include the day)
+		const firstDateDay = ( isInRange ) ? parseInt( dateQuery.split( "&" )[0].slice( 6, 8 ) ) : parseInt( dateQuery.slice( 6, 8 ) ); // the slice (6, 8) is used to get the day from the date string (YYYY-MM-DD) so 6 is the day of the month and 8 is the day of the month + 1 (so it doesn't include the day)
 
+		const secondDateYear = ( isInRange ) ? parseInt( dateQuery.split( "&" )[1].slice( 0, 4 ) ) : null // convert the string to a number and get the year from the date string (YYYY-MM-DD), null means that it is not in the range
+		const secondDateMonth = ( isInRange ) ? parseInt( dateQuery.split( "&" )[1].slice( 4, 6 ) ) : null
+		const secondDateDay = ( isInRange ) ? parseInt( dateQuery.split( "&" )[1].slice( 6, 8 ) ) : null
+
+		const firstDate = new Date( Date.UTC( firstDateYear, firstDateMonth - 1, firstDateDay ) ); // the month is -1 because the months are 0 indexed
+		const secondDate = ( isInRange ) ? new Date( Date.UTC( secondDateYear, secondDateMonth - 1, secondDateDay ) ) : null;
+
+		const transactions_data = ( isInRange ) ?
+			transactions.filter( transaction => ( new Date( trans_date ) >= firstDate && new Date( transaction.trans_date ) <= secondDate ) ) : // if it is in the range, then filter the transactions by the date range (>= and <=)
+			transactions.filter( transaction => ( new Date( transaction.trans_date ).getTime() === firstDate.getTime() ) ); // if it is not in the range, then filter the transactions by
+		return transactions_data;
+	}
+);
