9e9395c7e5330457e7f33eeae650ec6caa84d7bd
Eric Peltzman <epeltz@optonline.net>
2022-11-22 02:27:46 +0000

    get transactions by category

--- react-app/src/store/transaction/selectors.js
+++ react-app/src/store/transaction/selectors.js
@@ -7,8 +7,15 @@ const transactions = ( state ) => state.transaction.all;
 export const getTransactionsByAccount = createSelector(
 	[transactions( state, accountId )=> accountId],
 	( transactions, accountId ) => {
-		return transactions.filter( ( transaction ) => transaction.accountId === accountId );
+		return transactions.filter( ( transaction ) => transaction.accountId === +accountId ); // + converts string to number
 	}
+);
+
+export const getTransactionsByCategory = createSelector(
+	[transactions( state, categoryId )=> categoryId],
+    ( transactions, categoryId ) => {
+        return transactions.filter( ( transaction ) => transaction.categoryId === +categoryId );
+    }
 );
 
 
