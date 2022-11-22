8510e9313536c0b9fad8b8e56ebc12e056716d38
Eric Peltzman <epeltz@optonline.net>
2022-11-22 03:07:45 +0000

    transaction action types

--- react-app/src/store/transaction/transaction.js
+++ react-app/src/store/transaction/transaction.js
@@ -1,5 +1,22 @@
-const add_transaction = 'transactions/add_transaction'; // action type 
+const add_transaction = 'transactions/add_transaction'; // action type
 const delete_transaction = 'transactions/delete_transaction';
 const update_transaction = 'transactions/update_transaction';
 const load_transactions = 'transactions/load_transactions';
 const delete_account = 'transactions/delete_account'; //
+
+const add = ( newTransaction ) => {
+	return { type: add_transaction, newTransaction };
+};
+
+const delete = ( prevTransaction ) => {
+	return { type: delete_transaction, prevTransaction };
+};
+
+const update = ( transaction ) => {
+	return { type: update_transaction, transaction };
+};
+
+const load = (transactions) => {
+    return { type: load_transactions, transactions };
+};
+
