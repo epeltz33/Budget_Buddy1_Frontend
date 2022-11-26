9b0f0ba8e90b451a9db29fe71197a4825e04da0d
Eric-Peltzman33 <erpeltz@gmail.com>
2022-11-26 16:16:18 -0500

    backdrop for main page

---
+++ react-app/src/components/Backdrop/Backdrop.js
@@ -0,0 +1,19 @@
+import { motion } from 'framer-motion';
+import './Backdrop.css';
+
+
+const Backdrop = ({ onClick, children }) => {
+    return (
+        <motion.div
+        className="Backdrop"
+        onClick={onClick}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        >
+        {children}
+        </motion.div>
+    );
+    }
+
+export default Backdrop;
