f815a316f0a0b798f126aeebe026255a178ae7f3
epeltz33 <epeltz@optonline.net>
2022-12-01 22:09:34 -0500

    updated footer and env

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
-export default function Footer() { // export to be globally available to other files
-    return (
-        <div className="Footer">
-            <p> A Budgeting App by Eric Peltzman </p>
-            <span id ="github_link">
-                <a href="https://github.com/ericpeltzman">
-                    <i className="fab fa-github"></i>
-                </a>
-            </span>
-            <span id ="linkedin_link">
-                <a href="https://www.linkedin.com/in/eric-peltzman/"> 
-                    <i className="fab fa-linkedin"></i>
-                </a>
-                </span>
-        </div>
-    );
-};
-           
-                
+export default function Footer() {
+  // export to be globally available to other files
+  return (
+    <div className="Footer">
+      <p> A Budgeting App by Eric P </p>
+      <span id="github_link">
+        <a href="https://github.com/ericpeltzman">
+          <i className="fab fa-github"></i>
+        </a>
+      </span>
+      <span id="linkedin_link">
+        <a href="https://www.linkedin.com/in/eric-peltzman/">
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </span>
+    </div>
+  );
+}
